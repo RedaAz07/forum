875f89a52b5eb44c3145852a65c995d1484da270
ahmed <ahmed@baid.com>
2025-04-18 16:01:49 +0100

    style mab9a lih walo ajmii

--- helpers/GetUsernameFromSession.go
+++ helpers/GetUsernameFromSession.go
@@ -8,7 +8,7 @@ import (
 
 func GetUsernameFromSession(token string) string {
 	var username string
-	err := utils.Db.QueryRow("SELECT username FROM users WHERE session = ?", token).Scan(&username)
+	err := utils.Db.QueryRow("SELECT COALESCE(username, email) FROM users WHERE session = ?", token).Scan(&username)
 	if err != nil {
 		fmt.Println("GetUsernameFromSession error:", err)
 		return ""
