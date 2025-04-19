14eba14ffd96075593cc7b6a5de113e05113e0c7
Your Name <you@example.com>
2025-04-19 15:41:41 +0100

    fix db categories +

--- db/query.sql
+++ db/query.sql
@@ -1,60 +1,52 @@
-CREATE TABLE IF NOT EXISTS categories (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(50)
-);
+CREATE TABLE
+    IF NOT EXISTS categories (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(50)
+    );
 
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    session TEXT DEFAULT NULL
-);
+CREATE TABLE
+    IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        session TEXT DEFAULT NULL
+    );
 
+CREATE TABLE
+    IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username VARCHAR(30),
+        title VARCHAR(255),
+        description TEXT,
+        time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
 
+CREATE TABLE
+    IF NOT EXISTS comments (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        postID INTEGER,
+        username VARCHAR(30),
+        comment TEXT,
+        time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (postID) REFERENCES posts (id)
+    );
 
+CREATE TABLE
+    IF NOT EXISTS likes (
+        userID INTEGER,
+        postID INTEGER,
+        value VARCHAR(2),
+        PRIMARY KEY (userID, postID),
+        FOREIGN KEY (userID) REFERENCES users (id),
+        FOREIGN KEY (postID) REFERENCES posts (id)
+    );
 
-
-
-
-
-
-
-
-CREATE TABLE IF NOT EXISTS posts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username VARCHAR(30),
-    title VARCHAR(255),
-    description TEXT,
-    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    categoryID INTEGER,
-    FOREIGN KEY (categoryID) REFERENCES categories(id)
-);
-
-
-
-
-
-
-
-
-
-
-CREATE TABLE IF NOT EXISTS comments (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    postID INTEGER,
-    username VARCHAR(30),
-    comment TEXT,
-    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (postID) REFERENCES posts(id)
-);
-
-CREATE TABLE IF NOT EXISTS likes (
-    
-    userID INTEGER,
-    postID INTEGER,
-    value VARCHAR(2),
-    PRIMARY KEY (userID, postID),
-    FOREIGN KEY (userID) REFERENCES users(id),
-    FOREIGN KEY (postID) REFERENCES posts(id)
-);
+CREATE TABLE
+    IF NOT EXISTS categories_post (
+        categoryID INTEGER,
+        postID INTEGER,
+        PRIMARY KEY (categoryID, postID),
+        FOREIGN KEY (categoryID) REFERENCES categories (id),
+        FOREIGN KEY (postID) REFERENCES posts (id)
+    );
--- handlers/CreatePost.go
+++ handlers/CreatePost.go
@@ -3,70 +3,56 @@ package handlers
 import (
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"forum/helpers"
 	"forum/utils"
 )
 
 func CreatePost(w http.ResponseWriter, r *http.Request) {
-
 	// !  check the post method
 	if r.Method != "POST" {
 		helpers.RanderTemplate(w, "statusPage.html", http.StatusMethodNotAllowed, nil)
 		return
 
 	}
-// !  check the session that if the user is logged in
+	// !  check the session that if the user is logged in
 	exists, session := helpers.SessionChecked(w, r)
 	if !exists {
 		fmt.Println("session exists", exists)
 		http.Redirect(w, r, "/", 302)
 		return
 	}
-// !  get the data 
+	// !  get the data
+
+	err := r.ParseForm()
+	if err != nil {
+		helpers.RanderTemplate(w, "statusPage.html", http.StatusBadRequest, utils.ErrorBadReq)
+		return
+
+	}
 	title := r.FormValue("title")
 	description := r.FormValue("description")
-	category := r.FormValue("categoryID")//* if he just choose the category
-	createCategory := r.FormValue("categories")// * if he create a new category
-// !  check if the user create a new category
 
-if createCategory != ""  && category == "" {
-	//  insert the new category into the database
-	stmt := `insert into categories (name) values(?)`
-	_, err := utils.Db.Exec(stmt, createCategory)
-	if err != nil {
-		helpers.RanderTemplate(w, "statusPage.html", http.StatusInternalServerError, utils.ErrorInternalServerErr)
-		return
-	}
-	// get the id of the new category
-	stmt2 := `select id from categories where name = ?`
-	row := utils.Db.QueryRow(stmt2, createCategory)
-	err = row.Scan(&category)
-	if err != nil {
-		helpers.RanderTemplate(w, "statusPage.html", http.StatusInternalServerError, utils.ErrorInternalServerErr)
-		return
-	}
+	category := r.Form["tags"] //* if he just choose the category
 
-	 
-}
+	fmt.Println("category", category)
+	// !  check if the user create a new category
 
 
-	categoryID, _ := strconv.Atoi(category)
 
-	// ! get the username 
+
+
+
+
+	// ! get the username
 	stmt2 := `select  username from users where session = ?`
 	row := utils.Db.QueryRow(stmt2, session)
 	var username string
 	row.Scan(&username)
 
-	stmt := `insert into posts (title, description, categoryID, username) values(?, ?, ?, ?)`
+	stmt := `insert into posts (title, description, username) values(?, ?, ?)`
 
-	_, err := utils.Db.Exec(stmt, title, description, categoryID, username)
-	if err != nil {
-		helpers.RanderTemplate(w, "statusPage.html", http.StatusInternalServerError, utils.ErrorInternalServerErr)
-		return
+	utils.Db.Exec(stmt, title, description, username)
 
-	}
 	http.Redirect(w, r, "/", 302)
 }
