3b78c3da84013e0784e971915d2a9b3473ecdb8f
Your Name <you@example.com>
2025-04-28 14:32:50 +0100

    fix  comment in  a post that not exists

--- handlers/Comment.go
+++ handlers/Comment.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"forum/helpers"
@@ -15,15 +16,31 @@ func CommentHandler(w http.ResponseWriter, r *http.Request) {
 
 	_, session := helpers.SessionChecked(w, r)
 
+	stmt2 := `select username from users where session = ?`
+	query := utils.Db.QueryRow(stmt2, session)
 
-	stmt2 :=`select username from users where session = ?`
-	query  := utils.Db.QueryRow(stmt2, session)
-	
 	var username string
-	query.Scan(&username)
+	errr := query.Scan(&username)
+	if errr != nil {
+		helpers.RanderTemplate(w, "statusPage.html", http.StatusInternalServerError, utils.ErrorInternalServerErr)
+		return
+
+	}
 
 	postID := r.FormValue("postID")
 	comment := r.FormValue("comment")
+
+	stmt3 := `select id from posts where id = ?`
+	query3 := utils.Db.QueryRow(stmt3, postID)
+
+	var postID2 int
+	errrr := query3.Scan(&postID2)
+	if errrr != nil {
+		fmt.Println("Error scanning postID:", errrr)
+		helpers.RanderTemplate(w, "statusPage.html", http.StatusInternalServerError, utils.ErrorInternalServerErr)
+		return
+
+	}
 
 	stmt := `insert into comments (postID, comment, username ) values(?, ? ,?)`
 	_, err := utils.Db.Exec(stmt, postID, comment, username)
