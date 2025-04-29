1d53da13ae68d4b6891752bfe34ec44771f5d95b
ahmed <ahmed@baid.com>
2025-04-29 11:28:13 +0100

    deseign update

--- helpers/GetUsernameFromSession.go
+++ helpers/GetUsernameFromSession.go
@@ -1,8 +1,6 @@
 package helpers
 
 import (
-	"fmt"
-
 	"forum/utils"
 )
 
@@ -10,7 +8,7 @@ func GetUsernameFromSession(token string) string {
 	var username string
 	err := utils.Db.QueryRow("SELECT COALESCE(username, email) FROM users WHERE session = ?", token).Scan(&username)
 	if err != nil {
-		fmt.Println("GetUsernameFromSession error:", err)
+		// fmt.Println("GetUsernameFromSession error:", err)
 		return ""
 	}
 	return username
--- helpers/db.go
+++ helpers/db.go
@@ -24,10 +24,10 @@ func DataBase() {
 		log.Fatal("read error:", err)
 	}
 
-	// i should to use     status ENUM('depend', 'refus', 'success') NOT NULL look chatgpt
 	_, err = utils.Db.Exec(string(sqlfile))
 	if err != nil {
 		log.Fatal("exec error: ", err)
 	}
+	
 	fmt.Println("Queries executed successfully!")
 }
