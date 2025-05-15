22076298c77b986103b01a9781cc30be688ffdc3
AhmedBaid <abaid@example.com>
2025-05-15 13:31:46 +0100

    change db statuspage and handle image

--- helpers/AllCategories.go
+++ helpers/AllCategories.go
@@ -1,24 +1,12 @@
 package helpers
 
 import (
-	"html/template"
 	"net/http"
 
 	"forum/utils"
 )
 
 func AllCategories(w http.ResponseWriter) []utils.Categories {
-	Icons := []string{
-		`<i class="fa-solid fa-medal"></i>`,
-		`<i class="fa-solid fa-music"></i>`,
-		`<i class="fa-solid fa-film"></i>`,
-		`<i class="fa-solid fa-flask"></i>`,
-		`<i class="fa-solid fa-dumbbell"></i>`,
-		`<i class="fa-solid fa-microchip"></i>`,
-		`<i class="fa-solid fa-person-walking"></i>`,
-		`<i class="fa-solid fa-landmark"></i>`,
-	}
-
 	var categories []utils.Categories
 
 	stmt := `SELECT name, id FROM categories`
@@ -29,7 +17,6 @@ func AllCategories(w http.ResponseWriter) []utils.Categories {
 	}
 	defer rows.Close()
 
-	i := 0
 	for rows.Next() {
 		var category utils.Categories
 		err := rows.Scan(&category.Name, &category.Id)
@@ -37,8 +24,6 @@ func AllCategories(w http.ResponseWriter) []utils.Categories {
 			RanderTemplate(w, "statusPage.html", http.StatusInternalServerError, nil)
 			return nil
 		}
-		category.Icon = template.HTML(Icons[i%len(Icons)])
-		i++
 		categories = append(categories, category)
 	}
 
